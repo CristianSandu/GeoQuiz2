368364da13f06e9122fabbac28deca100eb65d11
CristianSandu <alin-cristinel.sandu@studenti.univr.it>
2019-05-04 14:27:11 +0200

    created layout, added to UI, added some strings, added id's to buttons, added listeners on buttons

--- app/src/main/java/com/example/geoquiz2/MainActivity.java
+++ app/src/main/java/com/example/geoquiz2/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.geoquiz2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Buttons
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the int ref to buttons from R using findViewById()
+        mTrueButton =  findViewById(R.id.true_button);
+        mFalseButton =  findViewById(R.id.false_button);
+
+        // setOnClickListener to buttons
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
