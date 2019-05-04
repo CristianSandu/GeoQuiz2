1c34ff7fc56d0f7c5511ac7eadf1a3bc46b76161
CristianSandu <alin-cristinel.sandu@studenti.univr.it>
2019-05-04 14:43:11 +0200

    added Toasts , ended chapter.1

--- app/src/main/java/com/example/geoquiz2/MainActivity.java
+++ app/src/main/java/com/example/geoquiz2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
             }
         });
     }
