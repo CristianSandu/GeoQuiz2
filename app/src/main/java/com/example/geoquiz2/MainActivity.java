1ed2307e1c956f52e9c306349f9d1ce68c74e9ee
CristianSandu <alin-cristinel.sandu@studenti.univr.it>
2019-05-04 16:20:06 +0200

    ended chapter.2

--- app/src/main/java/com/example/geoquiz2/MainActivity.java
+++ app/src/main/java/com/example/geoquiz2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
     // Buttons
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+
+    // question Text View
+    private TextView mQuestionTextView;
+    // the array of questions  with their answers
+    private Question[] mQuestionBank = new Question[]{
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+
+    // index on array of questions
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,22 +35,58 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // get the int ref to buttons from R using findViewById()
-        mTrueButton =  findViewById(R.id.true_button);
-        mFalseButton =  findViewById(R.id.false_button);
+        mTrueButton = findViewById(R.id.true_button);
+        mFalseButton = findViewById(R.id.false_button);
+        mNextButton = findViewById(R.id.next_button);
+
+        // setting text to question
+        mQuestionTextView = findViewById(R.id.question_text);
+
 
         // setOnClickListener to buttons
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
-
+        // added toasts when button clicked
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        //  Encapsulating with updateQuestion()
+        updateQuestion();
+    }
+
+    //  Encapsulating with updateQuestion()
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+//    This method will accept a boolean variable that identifies whether the user pressed TRUE or FALSE.
+//    Then, it will check the user’s answer against the answer in the current Question object. Finally, after
+//    determining whether the user answered correctly, it will make a Toast that displays the appropriate
+//    message to the user.
+    private void checkAnswer (boolean userPressedTrue){
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if (userPressedTrue == answerIsTrue) messageResId = R.string.correct_toast;
+        else messageResId = R.string.incorrect_toast;
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
     }
 }
