ef98e373ee36a3a36dc269d0778e92974a9a936d
CristianSandu <alin-cristinel.sandu@studenti.univr.it>
2019-05-07 17:53:53 +0200

    finished chapter 5.

--- app/src/main/java/com/example/geoquiz2/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz2/CheatActivity.java
@@ -1,13 +1,101 @@
 package com.example.geoquiz2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+//          What you are seeing here is that the ActivityManager maintains a back stack and that this back stack
+//        is not just for your application’s activities. Activities for all applications share the back stack, which is
+//        one reason the ActivityManager is involved in starting your activities and lives with the OS and not
+//        your application. The stack represents the use of the OS and device as a whole rather than the use of a
+//        single application.
 public class CheatActivity extends AppCompatActivity {
+
+    private static final String EXTRA_ANSWER_IS_TRUE = "com.bignerdranch.android.geoquiz.answer_is_true";
+    private static final String EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown";
+
+    private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+//        To retrieve the value from the extra, you will use: getBooleanExtra()
+//        The first argument is the name of the extra. The second argument of getBooleanExtra(…) is a default
+//        answer if the key is not found.
+//        Note that Activity.getIntent() always returns the Intent that started the activity. This is what you
+//        sent when calling startActivity(Intent).
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = findViewById(R.id.answer_text_view);
+        mShowAnswerButton = findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+                setAnswerShownResult(true);
+            }
+        });
+
+//        There are two methods you can call in the child activity to send data back to the parent:
+//        setResult(int resultCode, Intent data)
+//        Typically, the result code is one of two predefined constants: Activity.RESULT_OK or
+//        Activity.RESULT_CANCELED. (You can use another constant, RESULT_FIRST_USER, as an offset when
+//        defining your own result codes.)
+//        Setting result codes is useful when the parent needs to take different action depending on how the child
+//        activity finished.
+//        For example, if a child activity had an OK button and a Cancel button, the child activity would set a
+//        different result code depending on which button was pressed. Then the parent activity would take a
+//        different action depending on the result code.
+//                Calling setResult(…) is not required of the child activity. If you do not need to distinguish
+//        between results or receive arbitrary data on an intent, then you can let the OS send a default
+//        result code. A result code is always returned to the parent if the child activity was started with
+//        startActivityForResult(…). If setResult(…) is not called,d, then when the user presses the Back
+//        button, the parent will receive Activity.RESULT_CANCELED.
+
     }
+
+
+//    This static method allows you to create an Intent properly configured with the extras CheatActivity
+//    will need. The answerIsTrue argument, a boolean, is put into the intent with a private name using
+//    the EXTRA_ANSWER_IS_TRUE constant. You will extract this value momentarily. Using a newIntent(…)
+//    method like this for your activity subclasses will make it easy for other code to properly configure their
+//    launching intents.
+//    You only need one extra, but you can put multiple extras on an Intent if you need to. If you do, add
+//    more arguments to your newIntent(…) method to stay consistent with the pattern
+
+    public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
+        Intent intent = new Intent(packageContext, CheatActivity.class);
+        intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        return intent;
+    }
+
+
+//     in passing some specific data back to QuizActivity. So you
+//    are going to create an Intent, put an extra on it, and then call Activity.setResult(int, Intent) to
+//    get that data into QuizActivity’s hands.
+//    In CheatActivity, add a constant for the extra’s key and a private method that does this work. Then
+//    call this method in the SHOW ANSWER button’s listener.
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
+
+
+    public static boolean wasAnswerShown(Intent result) {
+        return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false);
+    }
+
+
+
 }
--- app/src/main/java/com/example/geoquiz2/MainActivity.java
+++ app/src/main/java/com/example/geoquiz2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.geoquiz2;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
+    private Button mCheatButton;
 
     // question Text View
     private TextView mQuestionTextView;
@@ -33,7 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     // adding a tag
     private static final String TAG = "QuizActivity";
-
+    // the cheat code activity constant
+    private static final int REQUEST_CODE_CHEAT = 0;
+    // decode if user saw the answer
+    private boolean mIsCheater;
 
 
 
@@ -64,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
         mFalseButton = findViewById(R.id.false_button);
         mNextButton = findViewById(R.id.next_button);
         mPrevButton = findViewById(R.id.prev_button);
+        mCheatButton = findViewById(R.id.cheat_button);
 
         // setting text to question
         mQuestionTextView = findViewById(R.id.question_text);
@@ -98,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 if (isQuestionAnswered[mCurrentIndex]){
                     mTrueButton.setEnabled(true);
                     mFalseButton.setEnabled(true);
@@ -126,6 +134,64 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                 When an activity calls startActivity(Intent), this call
+//                 is sent to the OS.In particular, it is sent to a part of the OS called the ActivityManager. The ActivityManager then
+//                 creates the Activity instance and calls its onCreate(Bundle) method
+//                 An intent is an object that a component can use to communicate with the OS. The only components
+//                you have seen so far are activities, but there are also services, broadcast receivers, and content
+//                providers.
+//                Intents are multipurpose communication tools, and the Intent class provides different constructors
+//                depending on what you are using the intent to do.
+//                Before starting the activity, the ActivityManager checks the package’s manifest for a declaration with
+//                the same name as the specified Class. If it finds a declaration, it starts the activity, and all is well. If it
+//                does not, you get a nasty ActivityNotFoundException, which will crash your app. This is why all of
+//                your activities must be declared in the manifest.
+//                When you create an Intent with a Context and a Class object, you are creating an explicit intent. You
+//                use explicit intents to start activities within your application.
+//                        It may seem strange that two activities within your application must communicate via the
+//                ActivityManager, which is outside of your application. However, this pattern makes it easy for an
+//                activity in one application to work with an activity in another application.
+//                When an activity in your application wants to start an activity in another application, you create an
+//                implicit intent.
+//                Intent intent = new Intent(MainActivity.this, CheatActivity.class);
+//                Intent.putExtra();
+//                To inform the CheatActivity of the answer to the current question, you will pass it the value of
+//                mQuestionBank[mCurrentIndex].isAnswerTrue()
+//                You will send this value as an extra on the Intent that is passed into startActivity(Intent).
+//                        Extras are arbitrary data that the calling activity can include with an intent.
+//                The OS forwards the intent to
+//                the recipient activity, which can then access the extras and retrieve the data
+//                An extra is structured as a key-value pair, like the one you used to save out the value of mCurrentIndex
+//                in QuizActivity.onSaveInstanceState(Bundle).
+//                        To add an extra to an intent, you use Intent.putExtra(…)
+//                The first argument is
+//                always a String key, and the second argument is the value, whose type will vary. It returns the Intent
+//                itself, so you can chain multiple calls if you need to.
+//                        Now you could return to QuizActivity and put the extra on the intent, but there is a better approach.
+//                        There is no reason for QuizActivity, or any other code in your app, to know the implementation
+//                details of what CheatActivity expects as extras on its Intent. Instead, you can encapsulate that work
+//                into a newIntent(…) method.
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+//                startActivity(intent);
+
+
+//                When you want to hear back from the child activity, you call startActivityForResult()
+//                The first parameter is the same intent as before. The second parameter is the request code. The request
+//                code is a user-defined integer that is sent to the child activity and then received back by the parent. It
+//                is used when an activity starts more than one type of child activity and needs to know who is reporting
+//                back. QuizActivity will only ever start one type of child activity, but using a constant for the request
+//                code is a best practice that will set you up well for future changes.
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+
+
+
+
         //  Encapsulating with updateQuestion()
         mQuestionTextView = MethodsHelper.updateQuestion(  mQuestionBank,  mCurrentIndex,  mQuestionTextView);
     }
@@ -143,9 +209,12 @@ public class MainActivity extends AppCompatActivity {
         isQuestionAnswered[mCurrentIndex] = false;
 
         int messageResId = 0;
-        if (userPressedTrue == answerIsTrue) messageResId = R.string.correct_toast;
-        else messageResId = R.string.incorrect_toast;
-
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
+        } else {
+            if (userPressedTrue == answerIsTrue) messageResId = R.string.correct_toast;
+            else messageResId = R.string.incorrect_toast;
+        }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
 
     }
@@ -197,5 +266,19 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "onSaveInstanceState");
         outState.putInt(KEY_INDEX, mCurrentIndex);
 
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 }
